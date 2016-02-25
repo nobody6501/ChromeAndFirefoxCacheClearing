ad2e12ee80cb2710026549c50006443de85360f7
nobody6501 <nobody6501@utexas.edu>
2016-02-25 15:39:00 -0600

    clean up

--- Browsers.Cache.and.Spotify.Wiping.bat
+++ Browsers.Cache.and.Spotify.Wiping.bat
@@ -6,6 +6,7 @@ if exist C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data goto :chrome
 set ChromeDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data
 rd /s /q "%ChromeDir%"
 
+
 if exist C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles goto :firefox
 :firefox
 set DataDir=C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles
@@ -18,23 +19,19 @@ for /d %%x in (C:\Users\%USERNAME%\AppData\Roaming\Mozilla\Firefox\Profiles\*) d
 if exist C:\Users\%USERNAME%\AppData\Local\Microsoft\ goto :IE
 :IE
 set DataDir=C:\Users\%USERNAME%\AppData\Local\Microsoft\Intern~1
-
 rd /s /q "%DataDir%"
 
 set History=C:\Users\%USER
 rd /s /q "%History%"
 
 set IETemp=C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Tempor~1
-
-REM del /q /s /f "%IETemp%"
 rd /s /q "%IETemp%"
 
 set Cookies=C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Cookies
-
-REM del /q /s /f "%Cookies%"
 rd /s /q "%Cookies%"
 
 C:\bin\regdelete.exe HKEY_CURRENT_USER "Software\Microsoft\Internet Explorer\TypedURLs"
 
 
+REM remove spotify
 if exist C:\Users\%USERNAME%\AppData\Local\Spotify\Storage rmdir /q /s C:\Users\%USERNAME%\AppData\Local\Spotify\Storage
